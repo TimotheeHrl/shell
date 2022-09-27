7a116231542b5319322236c896f91a1b6f5017fe
timotheehrl <tharleaux@gmail.com>
2022-09-27 12:11:29 +0200

    files for the test

--- files/createfiles.sh
+++ files/createfiles.sh
@@ -4,59 +4,56 @@
 
 declare -a persons
 persons=( "Lucie_Leroux" "Bernard_Tapie" "Leroy_Merlin" "Josiane_Lemieux" )
+checkStorageFolderExists(){
+         if [ ! -d "app" ]; then
+                echo "File App does not exist"
+                # create the file $person
+             mkdir "app"
+                   cd ./app/
+
+         else 
+                 cd ./app/
+                  
+         fi
+   }
+
+ 
+checkStorageFolderExists
 for person in ${persons[@]}
   do      
     echo "Person Name is $person"
+     
+createUserConfFile(){
+        mkdir conf
+        cd ./conf/
+        touch filesLocation.properties
+        echo "filesLocation.properties=$person" > filesLocation.conf
+        cd ..
+        }
 
-##### create persons with if else ... ####### 
-#     if [ ! -d "$person" ]; then
-#                 echo "File $person does not exist"
-#                 # create the file $person
-#                 mkdir $person
-#                 cd ./$person/
-#                 touch $person.txt
-#                 echo $person > $person.txt
-#                 cat ./$person.txt
-#                 cd ..
-#     else
-#                 cd ./$person/
-
-#        if [ -f $person.txt ]
-#         then
-               
-#                 echo "File $person exists"
-
-#                 cat ./$person.txt
-      
-#         else
-#                 echo "File $person does not exist"
-#                 touch $person.txt
-#                 echo $person > $person.txt
-#                 cat ./$person.txt
-#         fi 
-#         cd ..
-#         fi
-       
-##### create persons with functions ... ####### 
 checkIfPersonFolderExists(){
          if [ ! -d "$person" ]; then
                 echo "File $person does not exist"
                 # create the file $person
              createPersonFolder
+             
         else
                 cd ./$person/
                 echo pwd
                  cat ./$person.txt
                 cd ..
          fi
-
    }
+
+
    createPersonFolder(){
            mkdir $person
            cd ./$person/
            checkifPersonFileExist
+           createUserConfFile
                
    }
+  
    checkifPersonFileExist(){
          if [ -f $person.txt ]
         then
@@ -68,21 +65,72 @@ checkIfPersonFolderExists(){
                 createPersonFile
         fi
    }
-   createPersonFile(){
-         touch $person.txt
-        echo $person prout prout > $person.txt
-        readPersonFile        
-   }
+  
    readPersonFile(){
         cat ./$person.txt
-        cd .. 
+        cd ..
    }
-        
+     
      checkIfPersonFolderExists   
-done
 
+     cd ..
+    
+done
+ cd ..
+     echo "_____________________APP folder done_____________________________"
+     pwd
+
+  checkFicFolderExists(){
+         if [ ! -d "fic" ]; then
+                echo "File Fic does not exist"
+                # create the file $person
+             mkdir "fic"
+                   cd ./fic/
+                   # in bash how to get users ?
+         else 
+                 cd ./fic/
+                  
+         fi
+        }  
+
+checkFicFolderExists
+
+for person in ${persons[@]}
+  do      
+    echo "Person Name is $person"
+       createPersonFolderInFic(){
+           mkdir $person
+           cd ./$person/
+           mkdir integration
+           cd ./integration/
+           mkdir entree
+           mkdir OUT
+           mkdir error
+           cd ../..
+               
+   }
+
+   checkIfPersonFolderExistsInFic(){
+         if [ ! -d "$person" ]; then
+                echo "File $person does not exist"
+                # create the file $person
+             createPersonFolderInFic
+             
+        else
+                cd ./$person/
+                echo pwd
+                 cat ./$person.txt
+                cd ..
+         fi
+
+   }
+   checkIfPersonFolderExistsInFic
+pwd
+    done
 echo "Number of persons in Bucket is" ${#persons[@]}
 echo "All person ${persons[@]}"
- 
+
+
+
 
 
